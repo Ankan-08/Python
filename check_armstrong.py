bd07bf439af9de6db75b7b35d4bd242176315f80
Ankan Pal <ankanpal703@gmail.com>
2026-04-14 17:22:44 +0530

    Armstrong

---
+++ check_armstrong.py
@@ -0,0 +1,13 @@
+n:int=int(input("Enter a number: "))
+l=len(string(n))
+m=n
+sum:int=0
+while(m>0):
+  r=m%10
+  sum+=pow(r,l)
+  m//=10
+if(sum==n):
+  print(f"{n} is an Armstrong number")
+else:
+  print(f"{n} is not an Armstrong number")
+  
