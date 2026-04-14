6974240f555a8fc2c26924f4f807273cb57a8535
Ankan-08 <ankanpal703@gmail.com>
2026-04-14 17:02:45 +0530

    Add try catch

--- factorial.py
+++ factorial.py
@@ -1,5 +1,11 @@
 def fact(n:int)->int:
     if(n<=2): return n
     return n*fact(n-1)
-n=int(input("Enter a number: "))
-print(f"Factorial of {n} is {fact(n)}")
+flag : bool=True
+while(flag):
+    try:
+        n : int=int(input("Enter a number: "))
+        print(f"Factorial of {n} is {fact(n)}")
+        flag=False
+    except Exception as e:
+        print("Only Numbers are allowed")
